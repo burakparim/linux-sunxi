f8273b6dc8db3ff83c368e2a17f96a5767aa7f88
Burak <burak@senic.com>
2018-10-10 15:57:18 +0200

    ASoC:sun4i-i2s:Add slot width override This commit is inspired from the efforts Marcus Cooper <codekipper@gmail.com > showed and shared on linux-sunxi mailing list. This patch is created to test his efforts on a different linux kernel fork in order to test nano pi neo i2s, mainly for i2s capture mode. The original patches can be seen on the link below. https://groups.google.com/forum/#!topic/linux-sunxi/rTn02ry5Qmo Original commit/patch message is below.
    Signed-off-by: Ali Burak Parim <aliburakparim@gmail.com>
    --
    Some codecs require a different amount of a bit clocks per frame
    than what is calculated by using the sample width. Use a slot
    width override property to provide this mechanism.
    
    Signed-off-by: Marcus Cooper <codekipper@gmail.com>

--- sound/soc/sunxi/sun4i-i2s.c
+++ sound/soc/sunxi/sun4i-i2s.c
@@ -196,6 +196,8 @@ struct sun4i_i2s {
 	struct regmap_field	*field_rxchansel;
 
 	const struct sun4i_i2s_quirks	*variant;
+
+	unsigned int	slot_width;
 };
 
 struct sun4i_i2s_clk_div {
@@ -338,7 +340,7 @@ static int sun4i_i2s_set_clk_rate(struct sun4i_i2s *i2s,
 	if (i2s->variant->has_fmt_set_lrck_period)
 		regmap_update_bits(i2s->regmap, SUN4I_I2S_FMT0_REG,
 				   SUN8I_I2S_FMT0_LRCK_PERIOD_MASK,
-				   SUN8I_I2S_FMT0_LRCK_PERIOD(32));
+				   SUN8I_I2S_FMT0_LRCK_PERIOD(word_size));
 
 	/* Set sign extension to pad out LSB with 0 */
 	regmap_field_write(i2s->field_fmt_sext, 0);
@@ -407,8 +409,9 @@ static int sun4i_i2s_hw_params(struct snd_pcm_substream *substream,
 	regmap_field_write(i2s->field_fmt_sr,
 			   sr + i2s->variant->fmt_offset);
 
-	return sun4i_i2s_set_clk_rate(i2s, params_rate(params),
-				      params_width(params));
+	return sun4i_i2s_set_clk_rate(dai, params_rate(params),
+				      i2s->slot_width ?
+				      i2s->slot_width : params_width(params));
 }
 
 static int sun4i_i2s_set_fmt(struct snd_soc_dai *dai, unsigned int fmt)
@@ -1003,7 +1006,7 @@ static int sun4i_i2s_probe(struct platform_device *pdev)
 	struct sun4i_i2s *i2s;
 	struct resource *res;
 	void __iomem *regs;
-	int irq, ret;
+	int irq, ret, val;
 
 	i2s = devm_kzalloc(&pdev->dev, sizeof(*i2s), GFP_KERNEL);
 	if (!i2s)
@@ -1069,6 +1072,12 @@ static int sun4i_i2s_probe(struct platform_device *pdev)
 
 	i2s->capture_dma_data.addr = res->start + SUN4I_I2S_FIFO_RX_REG;
 	i2s->capture_dma_data.maxburst = 8;
+
+	if(!of_property_read_u32(pdev->dev.of_node,
+				"allwinner,slot-width-override", &val)) {
+		if(val >= 8 && val <= 32)
+			i2s->slot_width =val;
+	}
 
 	pm_runtime_enable(&pdev->dev);
 	if (!pm_runtime_enabled(&pdev->dev)) {
